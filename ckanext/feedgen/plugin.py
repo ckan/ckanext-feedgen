8dd5399e94a92da606001b1907245415446af8ce
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2026-04-03 23:39:30 +0300

    chore: add templates

--- ckanext/feedgen/plugin.py
+++ ckanext/feedgen/plugin.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+from typing_extensions import override
+
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
+from ckan.common import CKANConfig
 
 
 @tk.blanket.blueprints
 @tk.blanket.config_declarations
-class FeedgenPlugin(p.SingletonPlugin):
-    pass
+class FeedgenPlugin(p.IConfigurer, p.SingletonPlugin):
+    @override
+    def update_config(self, config: CKANConfig) -> None:
+        tk.add_template_directory(config, "templates")
